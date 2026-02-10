6baac1269be488ab66b30d9fdedaddaee34871d7
menahem_erlich <erlichmenahem@gmail.com>
2026-02-10 15:41:33 +0200

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,10 @@ function App() {
     <div className='body'>
       <Header />
       <main>
-        <ProfileCard avatar='../public/profil.jpg' name='Menahem Erlich' description='A student trying to show that he knows how to do something' personType='Student' />
-        <ProfileCard avatar='../public/2.webp' name='Etty Erlich' description='e0556735893@gmail.com' personType='Teacher' />
-        <ProfileCard avatar='../public/3.webp' name='Shaindy Erlich' description='scheindi@gmail.com' personType='Friend' />
-        <ProfileCard avatar='../public/4.webp' name='Israel Erlich' description='ierlich@gmail.com' personType='Friend' />
+        <ProfileCard avatar='/profil.jpg' name='Menahem Erlich' description='A student trying to show that he knows how to do something' personType='Student' />
+        <ProfileCard avatar='/2.webp' name='Etty Erlich' description='e0556735893@gmail.com' personType='Teacher' />
+        <ProfileCard avatar='/3.webp' name='Shaindy Erlich' description='scheindi@gmail.com' personType='Friend' />
+        <ProfileCard avatar='/4.webp' name='Israel Erlich' description='ierlich@gmail.com' personType='Friend' />
       </main>
       <Footer/>
     </div>
